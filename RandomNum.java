6e8b84dd993a8d4f7da7065a4a07a5dea35620dc
supersim3 <supersim3@gmail.com>
2014-03-27 13:39:12 +0800

    Create RandomNum.java

---
+++ RandomNum.java
@@ -0,0 +1,20 @@
+package randomnumberassesment;
+
+/**
+ *
+ * @author Simeon Dzieciol
+ */
+//get message box and prompt box classes
+import javax.swing.*;
+public class RandNum {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+	//initalise variables
+	
+	//use while loop with counter and answer variables
+    }
+}
